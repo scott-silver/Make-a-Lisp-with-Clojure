df2e07b30c7d35b66f5f7bb7fbb343a73f417463
Scott Silver <silversh@gmail.com>
2020-04-04 09:02:33 -0700

    consume-list, tests

--- src/make_a_lisp_with_clojure/reader.clj
+++ src/make_a_lisp_with_clojure/reader.clj
@@ -1,17 +1,36 @@
 (ns make-a-lisp-with-clojure.reader)
 
-;; next
+;; just treats everything as a string right now
+(defn consume-atom [atom]
+  [:string atom])
 
-;; peek
+;; consume-list should be given the remainder of a list, (not the opening
+;; paren). so if the list of tokens is '( \( \a \) ), then you should only pass
+;; '( \a \) ) to consume-list
+(defn consume-list
+  ([tokens]
+   (consume-list tokens []))
+  ([tokens accumulated-list]
+   (let [first-token (first tokens)]
+    (cond
+      ;; should throw an error if it runs out of tokens (because it means you're
+      ;; missing a close-paren
+      (empty? tokens)
+      (throw (Exception. "missing close-parens"))
 
-;; read-str
-;;   calls tokenize with string, pass the list of tokens to read-form
+      ;; return accumulated list when you hit a close parens
+      (= ")" first-token)
+      [[:list accumulated-list] (rest tokens)]
 
-;; read-form
+      ;; recur on consume-list when you discover a sub list
+      (= "(" first-token)
+      (let [[list remaining-tokens] (consume-list (rest tokens) accumulated-list)]
+        (recur remaining-tokens (conj accumulated-list list)))
 
-;; read-list
+      ;; pass all other tokens to consume-atom
+      :else
+      (recur (rest tokens) (conj accumulated-list (consume-atom first-token)))))))
 
-;; read-atom
 
 ;; tokenize
 (def token-regex #"[\s,]*(~@|[\[\]{}()'`~^@]|\"(?:[\\].|[^\\\"])*\"?|;.*|[^\s\[\]{}()'\"`@,;]+)")
--- test/make_a_lisp_with_clojure/reader_test.clj
+++ test/make_a_lisp_with_clojure/reader_test.clj
@@ -32,3 +32,48 @@
     (is (= (r/data-structure-for-token-list '("a")) [[:string "a"]]))
     (is (= (r/data-structure-for-token-list '("ab")) [[:string "ab"]]))
     (is (= (r/data-structure-for-token-list '("a" "b")) [[:string "a"] [:string "b"]]))))
+
+  ;; (testing "returns a :list for an empty list"
+  ;;   (is (= (r/data-structure-for-token-list '("(" ")")) [[:list []]]))))
+
+(deftest consume-list-test
+  (testing "throws an error if the list of tokens is missing a close-paren"
+    (is (thrown-with-msg? Exception #"missing close-parens" (r/consume-list '()))))
+
+  (testing "returns an empty list when it hits a close-paren `)`"
+    (is (= (r/consume-list '(")"))
+           [[:list []] '()])))
+
+  (testing "returns items before close-paren as contents of list"
+    (is (= (r/consume-list '("a" ")"))
+           [[:list [[:string "a"]]] '()])))
+
+  (testing "returns items after close-parens as the second return value"
+    (is (= (r/consume-list '(")" "a" "b" "c"))
+          [[:list []] '("a" "b" "c")]))
+
+    (is (= (r/consume-list '("a" ")" "b" "c"))
+          [[:list [[:string "a"]]] '("b" "c")])))
+
+  (testing "returns nested lists"
+    (is (= (r/consume-list '("(" ")" ")"))
+           [[:list [[:list []]]] '()]))
+
+    (is (= (r/consume-list '("(" "(" ")" ")" ")"))
+           [[:list [[:list [[:list []]]]]] ()]))
+
+    (is (= (r/consume-list '("(" "a" ")" ")"))
+           [[:list [[:list [[:string "a"]]]]] '()]))
+
+    (is (= (r/consume-list '("(" "a" ")" "b" ")"))
+           [[:list [[:list [[:string "a"]]] [:string "b"]]] '()]))
+
+    (is (= (r/consume-list '("(" "a" ")" "b" ")" "c"))
+           [[:list [[:list [[:string "a"]]] [:string "b"]]] '("c")]))
+
+    (is (= (r/consume-list '("(" "a" ")" "b" ")" "c" "d"))
+           [[:list [[:list [[:string "a"]]] [:string "b"]]] '("c" "d")]))
+
+    (is (= (r/consume-list '("(" "a" ")" "b" ")" "c" "d" "(" ")"))
+           [[:list [[:list [[:string "a"]]] [:string "b"]]] '("c" "d" "(" ")")]))))
+
