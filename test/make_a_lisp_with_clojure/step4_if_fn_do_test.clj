44c60708b0822a86e5e4693e28b60d307cd53eaf
Scott Silver <silversh@gmail.com>
2020-04-30 08:37:58 -0700

    closures test

--- test/make_a_lisp_with_clojure/step4_if_fn_do_test.clj
+++ test/make_a_lisp_with_clojure/step4_if_fn_do_test.clj
@@ -97,13 +97,11 @@
   (testing "user-defined functions"
     (is (= "7" (s4/read-eval-print "((fn (a b) (+ b a)) 3 4)")))
     (is (= "4" (s4/read-eval-print "( (fn () 4) )")))
-    (is (= "8" (s4/read-eval-print "( (fn (f x) (f x)) (fn (a) (+ 1 a)) 7 )")))))
+    (is (= "8" (s4/read-eval-print "( (fn (f x) (f x)) (fn (a) (+ 1 a)) 7 )"))))
 
-;;
-;; ;; Testing closures
-;; ( ( (fn* (a) (fn* (b) (+ a b))) 5) 7)
-;; ;=>12
-;;
+  (testing "closures"
+    (is (= "12" (s4/read-eval-print "(((fn (a) (fn (b) (+ a b))) 5) 7)")))))
+
 ;; (def! gen-plus5 (fn* () (fn* (b) (+ 5 b))))
 ;; (def! plus5 (gen-plus5))
 ;; (plus5 7)
